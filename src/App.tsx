97aba0f2771664f9c5c841cd76a9b95c3af208db
flashzbarry6-cell <flashzbarry6@gmail.com>
2025-10-07 17:42:35 -0700

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -94,3 +94,23 @@ const App = () => (
 );
 
 export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoanPage from './components/LoanPage';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoanPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
