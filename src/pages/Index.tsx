b07c17722e0b300c28758d9d7977e9fbee3be36b
flashzbarry6-cell <flashzbarry6@gmail.com>
2025-10-07 17:43:45 -0700

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -36,3 +36,15 @@ const Index = () => {
 };
 
 export default Index;
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
