41a171d7c0aa8aa6fa541363d1db2b1e0da23b16
flashzbarry6-cell <flashzbarry6@gmail.com>
2025-10-14 15:00:02 -0700

    Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -370,7 +370,7 @@ useEffect(() => {
   const services = [
     { icon: Users, label: "Support", bgClass: "bg-primary/10", route: "/support" },
     { icon: Calculator, label: "Investment", bgClass: "bg-primary/10", route: "/investment" },
-    { icon: Banknote, label: "Withdraw", bgClass: "bg-primary/10", route: "/withdrawal-amount" },
+   { icon: Gamepad2, label: "Spin & Win", bgClass: "bg-primary/10", route: "/spin-win" },
     { icon: CreditCard, label: "Airtime", bgClass: "bg-primary/10", route: "/buy-airtime" },
     { icon: Wifi, label: "Data", bgClass: "bg-primary/10", route: "/buy-data" },
     { icon: Banknote, label: "Loan", bgClass: "bg-primary/10", route: "/loan" },
