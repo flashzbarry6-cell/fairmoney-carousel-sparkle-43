892824b616873bdbf5523cc68dcbbad0821ae8e7
flashzbarry6-cell <flashzbarry6@gmail.com>
2025-10-20 08:43:50 -0700

    WithdrawalReceipt.tsx

--- src/pages/WithdrawalReceipt.tsx
+++ src/pages/WithdrawalReceipt.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft, AlertTriangle, X } from "lucide-react";
+ import { ArrowLeft, AlertTriangle, X } from "lucide-react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
